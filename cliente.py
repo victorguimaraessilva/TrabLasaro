77cd690cab47ddff482e81cb548ebcdb9023a7ae
victorguimas <victorguimaraes@ufu.br>
2018-09-21 19:11:38 -0300

    fixes

--- cliente.py
+++ cliente.py
@@ -11,26 +11,50 @@ def databaser(comand, key, value):
     print('[DB] Received: '+str(comand))
 
     if comand == 'select':
-        keys = database['keys']
-        for key in keys:
-            print('[IDX:'+str(key)+'] - '+str(database['bytes'][key])+'')
 
+        clear()
+        print("[DB] Listing:")
+
+        keys = database['keys']
+
+        if len(keys) > 0:
+            for key in keys:
+                print('[IDX:'+str(key)+'] - '+str(database['bytes'][key])+'')
+        else:
+            print("\n Tabela vazia")
 
     if comand == 'insert':
+
+        clear()
         print('[DB] Inserting: '+str(value))
+
         count = len(database['keys'])
         database['keys'].append(count)
         database['bytes'].append(bytearray(value, 'utf8'))
+        print("\n Registro Adicionado")
 
 
     if comand == 'update':
-        print('[DB] Updating Key: '+str(key))
-        database['bytes'][int(key)] = bytearray(value, 'utf8')
 
+        clear()
+        print('[DB] Updating Key: '+str(key))
+
+        try:
+            database['bytes'][int(key)] = bytearray(value, 'utf8')
+            print("\n Registro Atualizado")
+        except Exception:
+            print("\n Chave não encontrada")
 
     if comand == 'delete':
+        clear()
         print('[DB] Deleting Key: '+str(key))
-        database['bytes'][int(key)] = None
+
+        try:
+            database['bytes'][int(key)] = None
+            print("\n Registro Excluído")
+        except Exception:
+            print("\n Chave não encontrada")
+         
 
 
     #print('[DB] Key: '+str(key))
@@ -38,62 +62,69 @@ def databaser(comand, key, value):
 
 def menu():
 
-    count = 0
-
     stop = False
 
     while not stop:
-
+        print("")
+        print("")
+        print("-------------------------------")
         print('[1] - Listar registros')
         print('[2] - Adicionar registro')
         print('[3] - Atualizar registro')
         print('[4] - Excluir registro')
         print('[5] - Sair')
 
-        opt = input("Selecione um comando:")
+        opt = input("Selecione uma opção:")
 
         if opt == "1": 
-            print("\n Listando registros")
 
             thread2 = Thread(target = databaser, args = ('select','',''))
             thread2.start()
             thread2.join()
 
-
         elif opt == "2":
-            print("\n Registro Adicionado") 
             
-            thread3 = Thread(target = databaser, args = ('insert','','11'))
+            value = input("Valor a ser adicionado:")
+
+            thread3 = Thread(target = databaser, args = ('insert', '', value))
             thread3.start()
             thread3.join()
             
         elif opt == "3":
-            print("\n Registro Atualizado")
 
-            thread3 = Thread(target = databaser, args = ('update','2','22'))
+            key = input("Digite a chave a ser atualizada:")
+            value = input("Digite o novo valor:")
+
+            thread3 = Thread(target = databaser, args = ('update', key, value))
             thread3.start()
             thread3.join()
 
         elif opt == "4":
-            print("\n Registro Excluído") 
 
-            thread4 = Thread(target = databaser, args = ('delete','4',''))
+            key = input("Digite a chave a ser deletada:")
+
+            thread4 = Thread(target = databaser, args = ('delete', key, ''))
             thread4.start()
             thread4.join()
 
         elif opt == "5":
+
             print("\n Fechando conexão")
             stop = True 
+
         elif opt !="":
+
             print("\n Digite um comando válido") 
 
 
+def clear():
+    print(chr(27)+'[2j')
+    print('\033c')
+    print('\x1bc')
 
 if __name__ == "__main__":
 
     t1 = Thread(target = menu)
     t1.setDaemon(True)
     t1.start()
-
-    while True:
-      pass
+    t1.join()
