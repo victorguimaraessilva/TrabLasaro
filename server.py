0044f8f6e873faf1ee9668baac8bef7911cca3ea
Gilberto Magalhães Leite Filho <gil-m@users.noreply.github.com>
2018-10-08 11:13:56 -0300

    Mudando select e insert para receber a chave

--- server.py
+++ server.py
@@ -2,15 +2,9 @@
 from threading import Thread
 import socket
 import sys
-import os.path
-import os
 
 
-keys = []
-bytes = []
-database = {'keys': keys, 'bytes': bytes}
-operations = []
-
+database = {}
 
 def databaser(comand, key, value):
 
@@ -19,27 +13,23 @@ def databaser(comand, key, value):
     if comand == 'select':
         # limpa a tela para evitar excesso de informação
         clear()
-        print("[DB] SELECT:")
+        print("[DB] Listing:")
 
-        keys = database['keys']
-        response = []
+        msg = ''
 
-        if len(keys) > 0:
-            for key in keys:
-                msg = 'id {0}: <{1}>'.format(key, database['bytes'][key])
-                print(msg)
-                response.append(msg)
+        result = database.get(str(key))
+        if result:
+            msg = '[IDX:'+str(key)+'] - '+str(database[key])
         else:
-            msg = "Tabela vazia"
-            print(msg)
-            response = [msg]
+            msg = 'NOK - Chave ' + key + ' não tem valores atribuídos'
 
-        return ', '.join(response)
+        print(msg)
+        return msg
 
     if comand == 'insert':
         # limpa a tela para evitar excesso de informação
         clear()
-        print('[DB] INSERT: '+str(value))
+        print('[DB] Inserting: '+str(value))
 
         count = len(database['keys'])
         database['keys'].append(count)
@@ -47,10 +37,11 @@ def databaser(comand, key, value):
         print("\n Registro Adicionado")
         return str("\n Registro Adicionado")
 
+
     if comand == 'update':
         # limpa a tela para evitar excesso de informação
         clear()
-        print('[DB] UPDATE: key '+str(key))
+        print('[DB] Updating Key: '+str(key))
 
         try:
             database['bytes'][int(key)] = bytearray(value, 'utf8')
@@ -63,7 +54,7 @@ def databaser(comand, key, value):
     if comand == 'delete':
         # limpa a tela para evitar excesso de informação
         clear()
-        print('[DB] DELETE: key '+str(key))
+        print('[DB] Deleting Key: '+str(key))
 
         try:
             database['bytes'][int(key)] = None
@@ -73,91 +64,62 @@ def databaser(comand, key, value):
             print("\n Chave não encontrada")
             return str("\n Chave não encontrada")
 
-    # print('[DB] Key: '+str(key))
+    #print('[DB] Key: '+str(key))
 
 
 def server():
 
-    if os.path.isfile('db.txt'):
+    # cria um socket para comunicação via TCP
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # configura o socket para receber comandos na porta 10001
+    server_address = ('localhost', 10001)
+    print('Iniciando conexão na {} porta {}'.format(*server_address))
+    sock.bind(server_address)
+
+    # começa a escutar
+    sock.listen(1)
+
+    while True:
+
+        print('Esperando por conexões')
+        connection, client_address = sock.accept()
+
         try:
-            file = open('db.txt', 'r')
+            print('Conexão de', client_address)
 
-            for line in file.read().split('\n'):
-                message = line.split("/")
-                comand = message[0]
-                key = message[1]
-                value = message[2]
-                databaser(comand, key, value)
+            while True:
 
-            file.close()
-        except:
-            print('erro ao abrir arquivo')
+                # tenta ler o socket
+                data = connection.recv(1024)
 
-    file = open('db.txt', 'a')
+                print('recebido {!r}'.format(data))
 
-    try:
-        # cria um socket para comunicação via TCP
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(5)
+                if data:
+                    # caso tenha dados na conexão, tenta decodificar
+                    comands = data.decode("UTF-8")
+                    # separa os comandos para identificar o que deve ser feito
+                    message = str(comands).split("/")
+                    comand = message[0]
+                    key = message[1]
+                    value = message[2]
 
-        # configura o socket para receber comandos na porta 10001
-        server_address = ('localhost', 10001)
-        print('Iniciando conexão na {} porta {}'.format(*server_address))
-        sock.bind(server_address)
+                    # chama a função que executa os comandos
+                    back_message = databaser(comand, key, value)
 
-        # começa a escutar
-        sock.listen(1)
+                    # se o retorno for uma lista, transforma em string
+                    if (type(back_message) == list):
+                        back_message = ''.join(back_message)
 
-        while True:
+                    print('enviando dados para o cliente')
+                    connection.sendall(back_message.encode("UTF-8"))
+                else:
+                    print('no data from', client_address)
+                    break
 
-            print('Esperando por conexões')
-            connection, client_address = sock.accept()
-
-            try:
-                print('Conexão de', client_address)
-
-                while True:
-
-                    # tenta ler o socket
-                    data = connection.recv(1024)
-
-                    print('recebido {!r}'.format(data))
-
-                    if data:
-                        # caso tenha dados na conexão, tenta decodificar
-                        comands = data.decode("UTF-8")
-                        # separa os comandos para identificar o que deve ser feito
-                        message = str(comands).split("/")
-                        comand = message[0]
-                        key = message[1]
-                        value = message[2]
-
-                        if comand in ['insert', 'update', 'delete']:
-                            operations.append(comands)
-                        # chama a função que executa os comandos
-                        back_message = databaser(comand, key, value)
-
-                        # se o retorno for uma lista, transforma em string
-                        if (type(back_message) == list):
-                            back_message = ''.join(back_message)
-
-                        print('enviando dados para o cliente')
-                        connection.sendall(back_message.encode("UTF-8"))
-                    else:
-                        print('no data from', client_address)
-                        break
-
-            finally:
-                # fecha a conexão do socket
-                connection.close()
-    except:
-        print('Timeout de 5 segundos atingido')
-    finally:
-        if operations:
-            for operation in operations:
-                file.writelines(operation)
-            file.write('\n')
-        file.close()
+        finally:
+            # fecha a conexão do socket
+            connection.close()
 
 
 def clear():
@@ -170,7 +132,7 @@ def clear():
 if __name__ == "__main__":
     # Verifica se a execução do python começou nesse arquivo
 
-    t1 = Thread(target=server)
+    t1 = Thread(target = server)
     t1.setDaemon(True)
     t1.start()
     t1.join()
